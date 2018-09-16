74632498331af729f1fd8510f8d22cbd3c8c5132
Tim Jenness <tjenness@lsst.org>
2018-09-16 16:53:03 -0700

    Add simple HSC translator

---
+++ python/lsst/obs/metadata/translators/hsc.py
@@ -0,0 +1,85 @@
+# This file is part of obs_metadata.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Metadata translation code for HSC FITS headers"""
+
+__all__ = ("HscTranslator", )
+
+from .fits import FitsTranslator
+
+
+class HscTranslator(FitsTranslator):
+    """Metadata translator for HSC standard headers.
+    """
+
+    name = "HSC"
+    """Name of this translation class"""
+
+    supportedInstrument = "HSC"
+    """Supports the HSC instrument."""
+
+    _constMap = {"instrument": "HSC"}
+    """This translator only works for HSC."""
+
+    _trivialMap = {"physical_filter": "FILTER01"}
+    """One-to-one mappings"""
+
+    @classmethod
+    def canTranslate(cls, header):
+        """Indicate whether this translation class can translate the
+        supplied header.
+
+        There is no ``INSTRUME`` header in an HSC file, so this method
+        looks for HSC mentions in other headers.
+
+        Parameters
+        ----------
+        header : `dict`-like
+           Header to convert to standardized form.
+
+        Returns
+        -------
+        can : `bool`
+            `True` if the header is recognized by this class. `False`
+            otherwise.
+        """
+        for k in ("EXP-ID", "FRAMEID"):
+            if k in header:
+                if header[k].startswith("HSC"):
+                    return True
+        return False
+
+    @classmethod
+    def to_datetime_begin(cls, header):
+        # We know it is UTC
+        return cls._from_fits_date_string(header["DATE-OBS"], timeStr=header["UT"], scale="utc")
+
+    @classmethod
+    def to_datetime_end(cls, header):
+        # We know it is UTC
+        return cls._from_fits_date_string(header["DATE-OBS"], timeStr=header["UT-END"], scale="utc")
+
+    @classmethod
+    def to_abstract_filter(cls, header):
+        physical = cls.to_physical_filter(header)
+        if physical.startswith("HSC-"):
+            return physical[4:]
+        return None
---
+++ tests/test_subaru.py
@@ -0,0 +1,39 @@
+# This file is part of obs_metadata.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from helper import readTestFile
+from lsst.obs.metadata import ObservationInfo
+
+
+class HscTestCase(unittest.TestCase):
+
+    def testHscTranslator(self):
+        header = readTestFile("fitsheader-hsc.yaml")
+        v1 = ObservationInfo(header)
+        self.assertEqual(v1.instrument, "HSC")
+        self.assertEqual(v1.telescope, "Subaru")
+        self.assertEqual(v1.abstract_filter, "i")
+
+
+if __name__ == "__main__":
+    unittest.main()
