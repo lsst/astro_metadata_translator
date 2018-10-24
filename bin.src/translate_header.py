5422373fcb892d66463d25eae0a1c5c75a315ac8
Tim Jenness <tjenness@lsst.org>
2018-10-24 09:01:11 -0700

    Add command line arguments to translate_header

--- bin.src/translate_header.py
+++ bin.src/translate_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 from astro_metadata_translator import ObservationInfo
 
 # Prefer afw over Astropy
@@ -14,15 +14,25 @@ except ImportError:
         fits_file = fits.open(file)
         return fits_file[hdu].header
 
+parser = argparse.ArgumentParser(description="Summarize headers from astronomical data files")
+parser.add_argument("files", metavar="file", type=str, nargs="+",
+                    help="File(s) from which headers will be parsed")
+parser.add_argument("-q", "--quiet", const=True, default=False, action="store_const",
+                    help="Do not report the summary for each file")
+
+args = parser.parse_args()
+print(repr(args))
 
 failed = []
-for file in sys.argv[1:]:
-    print(f"{file}:")
-    md = read_metadata(file)
+for file in args.files:
+    print(f"Analyzing {file}...")
     try:
+        md = read_metadata(file)
         obs_info = ObservationInfo(md, pedantic=True)
-        print(f"{obs_info}")
-    except Exception:
+        if not args.quiet:
+            print(f"{obs_info}")
+    except Exception as e:
+        print(e)
         failed.append(file)
 
 if failed:
