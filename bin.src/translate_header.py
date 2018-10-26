27b7ea12e2f7d5925af3264fc5c46ea7e8abb655
Tim Jenness <tjenness@lsst.org>
2018-10-26 13:24:32 -0700

    Add --dumphdr and --traceback options to translate_header

--- bin.src/translate_header.py
+++ bin.src/translate_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+import sys
+import traceback
 import argparse
+import yaml
 from astro_metadata_translator import ObservationInfo
 
 # Prefer afw over Astropy
@@ -18,24 +21,33 @@ parser = argparse.ArgumentParser(description="Summarize headers from astronomica
 parser.add_argument("files", metavar="file", type=str, nargs="+",
                     help="File(s) from which headers will be parsed")
 parser.add_argument("-q", "--quiet", const=True, default=False, action="store_const",
-                    help="Do not report the summary for each file")
+                    help="Do not report the translation content from each header")
+parser.add_argument("-d", "--dumphdr", const=True, default=False, action="store_const",
+                    help="Dump the header in YAML format to standard output rather than translating it")
+parser.add_argument("--traceback", const=True, default=False, action="store_const",
+                    help="Give detailed trace back when any errors encountered")
 
 args = parser.parse_args()
-print(repr(args))
 
 failed = []
 for file in args.files:
-    print(f"Analyzing {file}...")
+    print(f"Analyzing {file}...", file=sys.stderr)
     try:
         md = read_metadata(file)
+        if args.dumphdr:
+            print(yaml.dump(md))
+            continue
         obs_info = ObservationInfo(md, pedantic=True)
         if not args.quiet:
             print(f"{obs_info}")
     except Exception as e:
-        print(e)
+        if args.traceback:
+            traceback.print_exc(file=sys.stdout)
+        else:
+            print(repr(e))
         failed.append(file)
 
 if failed:
-    print("Files with failed translations:")
+    print("Files with failed translations:", file=sys.stderr)
     for f in failed:
-        print(f"\t{f}")
+        print(f"\t{f}", file=sys.stderr)
