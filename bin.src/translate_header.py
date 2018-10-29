babd8577637905b13bd75779b1f62cd4da8cb679
Tim Jenness <tjenness@lsst.org>
2018-10-29 09:38:26 -0700

    Allow directories to be read by translate_header

--- bin.src/translate_header.py
+++ bin.src/translate_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+import os
 import sys
 import traceback
 import argparse
@@ -19,7 +21,9 @@ except ImportError:
 
 parser = argparse.ArgumentParser(description="Summarize headers from astronomical data files")
 parser.add_argument("files", metavar="file", type=str, nargs="+",
-                    help="File(s) from which headers will be parsed")
+                    help="File(s) from which headers will be parsed."
+                    " If a directory is given it will be scanned for files matching the regular"
+                    " expression defined in --regex.")
 parser.add_argument("-q", "--quiet", const=True, default=False, action="store_const",
                     help="Do not report the translation content from each header")
 parser.add_argument("-d", "--dumphdr", const=True, default=False, action="store_const",
@@ -27,16 +31,21 @@ parser.add_argument("-d", "--dumphdr", const=True, default=False, action="store_
 parser.add_argument("--traceback", const=True, default=False, action="store_const",
                     help="Give detailed trace back when any errors encountered")
 
+re_default = r"\.fit[s]?\b"
+parser.add_argument("--regex", "-r", default=re_default,
+                    help="When looking in a directory, regular expression to use to determine whether"
+                    f" a file should be examined. Default: '{re_default}'")
+
 args = parser.parse_args()
 
-failed = []
-for file in args.files:
+
+def read_file(file, failed):
     print(f"Analyzing {file}...", file=sys.stderr)
     try:
         md = read_metadata(file)
         if args.dumphdr:
             print(yaml.dump(md))
-            continue
+            return
         obs_info = ObservationInfo(md, pedantic=True)
         if not args.quiet:
             print(f"{obs_info}")
@@ -46,6 +55,18 @@ for file in args.files:
         else:
             print(repr(e))
         failed.append(file)
+
+
+file_regex = re.compile(args.regex)
+failed = []
+for file in args.files:
+    if os.path.isdir(file):
+        for name in os.listdir(file):
+            path = os.path.join(file, name)
+            if os.path.isfile(path) and file_regex.search(name):
+                read_file(path, failed)
+    else:
+        read_file(file, failed)
 
 if failed:
     print("Files with failed translations:", file=sys.stderr)
