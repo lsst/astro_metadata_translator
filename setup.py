70bd8359d98afa2602f264a5e48b4e4c12d6a610
Stuart Mumford <stuart@cadair.com>
2018-12-06 10:35:28 -0700

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+version = "0.1.0"
+with open("./python/astro_metadata_translator/version.py", "w") as f:
+    f.write(f"__version__='{version}'")
+
+setup(
+    name='astro_metadata_translator',
+    version=f"{version}",
+    description='A translator for astronomical metadata.',
+    long_description="",
+    author='AURA/LSST',
+    author_email='tjenness@lsst.org',
+    url='https://github.com/lsst/astro_metadata_translator',
+    packages=['astro_metadata_translator'],
+    package_dir={'':'python'},
+    license='GPLv3',
+    install_requires=[
+        'astropy'
+    ],
+    tests_require=['pytest', 'responses',],
+    scripts=["bin.src/translate_header.py"]
+)
