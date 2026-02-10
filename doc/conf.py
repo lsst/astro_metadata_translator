dddedde7f806c91856ce245aa1e8d1f63949cb0a
Tim Jenness <tjenness@lsst.org>
2026-02-10 10:29:10 -0700

    Try new documenteer
    The old documenteer can not be used with new pydantic.

--- doc/conf.py
+++ doc/conf.py
@@ -2,39 +2,8 @@
 
 This configuration only affects single-package Sphinx documenation builds.
 """
+# ruff: noqa: F403, F405
 
-from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
+from documenteer.conf.guide import *
 
-project = "astro_metadata_translator"
-html_theme_options["logotext"] = project  # noqa: F405, unknown name
-html_title = project
-html_short_title = project
-doxylink = {}
-
-# Remove astro_metadata_translator from the default intersphinx configuration
-try:
-    del intersphinx_mapping["astro_metadata_translator"]  # noqa
-except KeyError:
-    pass
-
-# As a temporary hack until we move to documenteer 2 delete scipy
-# (since it no longer works)
-try:
-    del intersphinx_mapping["scipy"]  # noqa: F405
-except KeyError:
-    pass
-
-# Add pipelines.lsst.io to the intersphinx configuration.
-# NOTE: we might want to be more sophisticated about mapping corresponding
-# versions of the Pipelines and astro_metadata_translator. This technique will
-# mostly work if the Pipelines and astro_metadata_translator are developed
-# concurrently.
-intersphinx_mapping["lsst"] = ("https://pipelines.lsst.io/v/daily/", None)  # noqa
-
-nitpick_ignore_regex = [
-    ("py:.*", r"lsst\..*"),  # Ignore warnings from links to other lsst packages.
-    ("py:class", "(None|item) -- remove.*"),  # MutableSequence
-    ("py:class", "integer -- return.*"),  # Sequence
-    ("py:class", "ResourcePathExpression"),
-    ("py:class", "ResourcePath"),
-]
+exclude_patterns.append("changes/*")
